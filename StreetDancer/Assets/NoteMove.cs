88d6c187a2e310f7ad67ee6fd122cfcc6d82ad0e
Lucas <lucas.gomes.de.paiva@gmail.com>
2018-09-29 01:38:07 -0300

    commit inicial

---
+++ StreetDancer/Assets/NoteMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMove : MonoBehaviour {
+
+	public float noteSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(-noteSpeed, 0f, 0f) * Time.fixedDeltaTime;
+	}
+}
---
+++ StreetDancer/Assets/Scripts/RythmBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RythmBar : MonoBehaviour {
+	public float bpm; // bpm da música, batimentos por minuto
+	public float noteSpeed; // velocidde que a nota vai em direcao da caixa de hit da nota
+	public GameObject novaNota; // objeto nota 
+	private float deltaTime; // tewmpo entre a ultima nota gerada e tempo atual
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaTime += Time.deltaTime;
+		if(deltaTime >= 60/bpm){
+			deltaTime = 0;
+			var nota = GameObject.Instantiate(novaNota);
+			nota.GetComponent<NoteMove>().noteSpeed = this.noteSpeed;
+		}
+	}
+}
