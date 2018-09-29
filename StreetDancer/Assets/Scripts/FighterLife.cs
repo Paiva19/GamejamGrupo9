c8e4892bb90bb690a6d56d73f341904c07fe0aed
John2509 <jmarcelofo@gmail.com>
2018-09-29 03:18:22 -0300

    Prefab de lutador e barras de vida

---
+++ StreetDancer/Assets/Scripts/FighterLife.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterLife : MonoBehaviour {
+
+    public int fullLife = 100;
+    private int life;
+    public int fisicalDamage;
+    public int musicalDamage;
+
+    public Slider lifeBar;
+
+    private float deltaTime;
+
+    public 
+	// Use this for initialization
+	void Start () {
+        life = fullLife;
+        lifeBar.maxValue = fullLife;
+        lifeBar.value = fullLife;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime += Time.deltaTime;
+        if (deltaTime > 2)
+        {
+            getDamaged(10);
+            deltaTime = 0f;
+        }
+    }
+
+    public void getDamaged(int damage)
+    {
+        this.life -= damage;
+        lifeBar.value = life;
+        if (life <= 0)
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
