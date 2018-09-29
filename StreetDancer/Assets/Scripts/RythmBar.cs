2e43ebd99856938019723b3fde78ec8ad66151e8
John2509 <jmarcelofo@gmail.com>
2018-09-29 09:46:48 -0300

    Cena inicial

--- StreetDancer/Assets/Scripts/RythmBar.cs
+++ StreetDancer/Assets/Scripts/RythmBar.cs
@@ -8,7 +8,7 @@ public class RythmBar : MonoBehaviour {
 	public float bpm = 95; // bpm da música, batimentos por minuto
 	public float noteSpeed = 4; // velocidde que a nota vai em direcao da caixa de hit da nota
 	public GameObject newNota; // objeto nota 
-	public Vector3 notePosition = new Vector3(9, -3.8f, 0);
+	public float notePosition = 9;
 	public KeyCode playNote; // Tecla para tocar alguma nota
 
 	private float deltaTime; // tempo entre a ultima nota gerada e tempo atual
@@ -24,7 +24,7 @@ public class RythmBar : MonoBehaviour {
 		if(deltaTime >= 60/bpm){
 			deltaTime = 0;
 			var note = GameObject.Instantiate(newNota);
-			note.GetComponent<Transform>().localPosition = notePosition;
+			note.GetComponent<Transform>().localPosition = GetComponent<Transform>().position + new Vector3(notePosition, 0, 0);
 			note.GetComponent<NoteMove>().noteSpeed = this.noteSpeed;
 		}
 	}
