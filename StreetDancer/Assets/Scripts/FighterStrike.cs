4c3d185dbf8d1a6901b75f24e3ab91016446b6aa
Lucas <lucas.gomes.de.paiva@gmail.com>
2018-09-29 14:48:34 -0300

    Criado dano extra para combos variados e bons

--- StreetDancer/Assets/Scripts/FighterStrike.cs
+++ StreetDancer/Assets/Scripts/FighterStrike.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FighterStrike : MonoBehaviour
 {
 	public int baseDamage; //Dano base que o golpe causa
+	public List<KeyValuePair<int,int>> comboFlow = new List<KeyValuePair<int, int>>();
 	public GameObject targetPlayer;
 	// Use this for initialization
 	void Start () {
-		
+		comboFlow.Clear();
 	}
 	
 	// Update is called once per frame
@@ -16,14 +19,69 @@ public class FighterStrike : MonoBehaviour
 
 	}
 
-	public void Strike(/*int atk,*/ int perfection)
+	public void Strike(int atk, int perfection)
 	// atk = numero do ataque utilizado, perfection: 0 = miss, 1 = bad, 2 = good, 3 = perfect
 	{
         //Calcula dano do ataque e aplica no adversario
-        Debug.Log("Chegou");
-
         int atkDamage;
-		atkDamage = baseDamage * perfection / 3;
+		if (comboFlow.Count < 7)
+		{
+			atkDamage = baseDamage * perfection / 3;
+			if (perfection >= 2)
+			{
+				comboFlow.Add(new KeyValuePair<int, int>(atk, perfection));
+			}
+			else
+			{
+				comboFlow.Clear();
+			}
+		}
+		else
+		{
+			atkDamage = FinishComboFlow();
+		}
 		targetPlayer.GetComponent<FighterLife>().getDamaged(atkDamage);
+
 	}
+
+	public int FinishComboFlow()
+	{
+		float mediaPerfection = 0;
+		double variance = 0;
+		int transicoes;
+		List <int> values = new List<int>();
+		comboFlow.ForEach(x => mediaPerfection += x.Value);
+		mediaPerfection = mediaPerfection / 7;
+		comboFlow.ForEach(x => values.Add(x.Key));
+		variance = GetVariance(values);
+		transicoes = calculaTransicoes(values);
+		comboFlow.Clear();
+		int dano = (int)Math.Round( transicoes * variance* baseDamage * mediaPerfection/8);
+		Debug.Log(dano);
+		return dano;
+	}
+
+	private int calculaTransicoes(List <int> values)
+	{
+		HashSet<int> transicoes = new HashSet<int>();
+		int i;
+		for (i = 1; i < values.Count; i++)
+		{
+			transicoes.Add((values[i - 1] * values[i - 1] + values[i]));
+		}
+
+		return transicoes.Count;
+	}
+	
+	private double GetVariance(List<int> doubleList)
+	{
+		double average = doubleList.Average();
+		double sum = 0;
+		int i;
+		for (i = 0; i < doubleList.Count; i++)
+		{
+			sum += (doubleList[i] - average) * (doubleList[i] - average);
+		}
+		return sum / i;
+	}  
 }
--- StreetDancer/Assets/Scripts/HitNote.cs
+++ StreetDancer/Assets/Scripts/HitNote.cs
@@ -21,13 +21,13 @@ public class HitNote : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
         bool hit = false;
-        KeyCode hitKey = default(KeyCode);
+		int hitKey = 0;
         foreach (KeyCode key in keyCodes)
         {
             hit = Input.GetKeyDown(key);
             if (hit)
             {
-                hitKey = key;
+                hitKey = keyCodes.IndexOf(key) + 1;
                 break;
             }
         }
@@ -36,24 +36,24 @@ public class HitNote : MonoBehaviour
 			if (perfectArea.GetComponent<HitArea>().hasNoteInside)
 			{
 				//Debug.Log("PERFECT " + hitKey.ToString());
-                player.GetComponent<FighterStrike>().Strike(3);
+                player.GetComponent<FighterStrike>().Strike(hitKey, 3);
 				Destroy(perfectArea.GetComponent<HitArea>().note);
 			}
 			else if (goodArea.GetComponent<HitArea>().hasNoteInside)
 			{
 				//Debug.Log("GOOD " + hitKey.ToString());
-                player.GetComponent<FighterStrike>().Strike(2);
+                player.GetComponent<FighterStrike>().Strike(hitKey, 2);
                 Destroy(goodArea.GetComponent<HitArea>().note);
 			}
 			else if (badArea.GetComponent<HitArea>().hasNoteInside)
 			{
 				//Debug.Log("BAD " + hitKey.ToString());
-                player.GetComponent<FighterStrike>().Strike(1);
+                player.GetComponent<FighterStrike>().Strike(hitKey, 1);
                 Destroy(badArea.GetComponent<HitArea>().note);
 			}
 			else
 			{
-                player.GetComponent<FighterStrike>().Strike(0);
+                player.GetComponent<FighterStrike>().Strike(1, 0);
                 //Debug.Log("YOU SUCK");
 			}
 		}
